4f85e7606de6a41217f08ea0a23ae8e7dba7a4d5
bbz <2632141215@qq.com>
2024-12-24 12:54:02 +0800

    feat: 通过进程+命名空间实现容器隔离，父通过管道传参给子

---
+++ main_command.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+
+	log "github.com/sirupsen/logrus"
+	"github.com/urfave/cli"
+	"tinydocker/container"
+)
+
+var runCommand = cli.Command{
+	Name: "run",
+	Usage: `Create a container with namespace and cgroups limit
+			mydocker run -it [command]`,
+	Flags: []cli.Flag{
+		cli.BoolFlag{
+			Name:  "it", // 简单起见，这里把 -i 和 -t 参数合并成一个
+			Usage: "enable tty",
+		},
+	},
+	/*
+		这里是run命令执行的真正函数。
+		1.判断参数是否包含command
+		2.获取用户指定的command
+		3.调用Run function去准备启动容器:
+	*/
+	Action: func(context *cli.Context) error {
+		if len(context.Args()) < 1 {
+			return fmt.Errorf("missing container command")
+		}
+		cmd := context.Args().Get(0)
+		tty := context.Bool("it")
+		Run(tty, []string{cmd})
+		return nil
+	},
+}
+var initCommand = cli.Command{
+	Name:  "init",
+	Usage: "Init container process run user's process in container. Do not call it outside",
+	Action: func(context *cli.Context) error {
+		log.Infof("init come on")
+		err := container.RunContainerInitProcess()
+		return err
+	},
+}
---
+++ run.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"os"
+	"strings"
+
+	log "github.com/sirupsen/logrus"
+	"tinydocker/container"
+)
+
+// Run 执行具体 command
+/*
+这里的Start方法是真正开始执行由NewParentProcess构建好的command的调用，它首先会clone出来一个namespace隔离的
+进程，然后在子进程中，调用/proc/self/exe,也就是调用自己，发送init参数，调用我们写的init方法，
+去初始化容器的一些资源。
+*/
+func Run(tty bool, comArray []string) {
+	parent, writePipe := container.NewParentProcess(tty)
+	if parent == nil {
+		log.Errorf("New parent process error")
+		return
+	}
+	if err := parent.Start(); err != nil {
+		log.Errorf("Run parent.Start err:%v", err)
+	}
+	// 在子进程创建后通过管道来发送参数
+	sendInitCommand(comArray, writePipe)
+	_ = parent.Wait()
+}
+
+// sendInitCommand 通过writePipe将指令发送给子进程
+func sendInitCommand(comArray []string, writePipe *os.File) {
+	command := strings.Join(comArray, " ")
+	log.Infof("command all is %s", command)
+	_, _ = writePipe.WriteString(command)
+	_ = writePipe.Close()
+}
